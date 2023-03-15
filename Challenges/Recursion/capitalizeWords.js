d38336a55f4b51e557da3064003691f3a050b566
JD Villarroel <jdvillarroel@gmail.com>
2023-03-15 07:04:04 -0500

    Solution for the challenge capitalizeWords added. Solution using recursion. TODO: try helper function method with recursion.

--- Challenges/Recursion/capitalizeWords.js
+++ Challenges/Recursion/capitalizeWords.js
@@ -26,15 +26,29 @@ function capitalizeWords(array) {
   /**
    * For every element of the array we check if the element is a
    * string. If so, we make a recursive call until the length of
-   * the string is 1 then we capitalize it and concatenate the rest
-   * of the characters of the string.
+   * the array is 1 then we capitalize it and concatenate the rest
+   * of the characters of the array.
    */
 
-  if (array.length() === 1) {
-    return array[array.length - 1].toUpperCase();
-  } else {
+  if (array.length === 1) {
+    return [array[0].toUpperCase()];
   }
+
+  let result = capitalizeWords(array.slice(0, -1));
+
+  /**
+   * At this point, we went back one level into the stack, so the
+   * array is one element longer. Then, we take the last element
+   * and push it into the results array.
+   */
+  result.push(array[array.length - 1].toUpperCase());
+
+  /**
+   * Return the current array back one level into the stack until
+   * we return from all the recursive calls.
+   */
+  return result;
 }
 
 let words = ["i", "am", "learning", "recursion"];
-capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
+capitalizeWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
