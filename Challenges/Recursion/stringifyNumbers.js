d3415ae381e21fd0d717668561dae7623a2c5489
JD Villarroel <jdvillarroel@gmail.com>
2023-03-16 11:56:10 -0500

    Add new challenge (recursion). The challenge stringifyNumbers. The function takes an object and return the same object with all numerical values as strings.

---
+++ Challenges/Recursion/stringifyNumbers.js
@@ -0,0 +1,11 @@
+/**
+ * Write a function called stringifyNumbers which takes in
+ * an object and finds all of the values which are numbers
+ * and converts them to strings. Recursion would be a great
+ * way to solve this!
+ *
+ * It should return the same object with all number values
+ * as strings.
+ */
+
+function stringifyNumbers(obj) {}
