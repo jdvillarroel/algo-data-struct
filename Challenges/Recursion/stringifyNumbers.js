de3112b9f2983f567b74b4c4fa0ee5312014298c
JD Villarroel <jdvillarroel@gmail.com>
2023-03-16 11:59:52 -0500

    Example object and expected solution added to the stringifyNumbers challenge.

--- Challenges/Recursion/stringifyNumbers.js
+++ Challenges/Recursion/stringifyNumbers.js
@@ -9,3 +9,31 @@
  */
 
 function stringifyNumbers(obj) {}
+
+let obj = {
+  num: 1,
+  test: [],
+  data: {
+    val: 4,
+    info: {
+      isRight: true,
+      random: 66,
+    },
+  },
+};
+
+stringifyNumbers(obj);
+
+/*
+{
+    num: "1",
+    test: [],
+    data: {
+        val: "4",
+        info: {
+            isRight: true,
+            random: "66"
+        }
+    }
+}
+*/
