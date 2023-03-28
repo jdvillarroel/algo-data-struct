e8cdd2efba64b555bc830ab719d3fe084c98c4ca
JD Villarroel <jdvillarroel@gmail.com>
2023-03-28 11:57:15 -0500

    Add optimization of the bubble sort algorithm to stop scanning the array when no swap have been done (which means is sorted).

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -1,35 +1,51 @@
 /**
  * Implement the bubble sort algorithm.
  */
-
-function swap(array, idx1, idx2) {
-  [array[idx1], array[idx2]] = [array[idx2], array[idx1]];
-}
+/**
+ * Since in every iteration of the bubble sort algorithm the biggest(or smallest)
+ * element is placed in its correct position, it is not necesary to include it in
+ * the next iteration. The outer loop help to do this by reducing by one in every
+ * iteration.
+ *
+ * The inner loop compares the current element with the next element. If the current
+ * is greater (or smaller) the elements are swapped and we point to the next
+ * element. This is done until the end of the array minus one since we look ahead
+ * one element of the current, so there is no point in selecting the last element
+ * of the array.
+ *
+ * After looping through the entire array, we return it (its sorted at the end).
+ */
 
 function bubbleSort(array, isAscending = true) {
   if (array.length === 0) return [];
-  /**
-   * Since in every iteration of the bubble sort algorithm the biggest(or smallest)
-   * element is placed in its correct position, it is not necesary to include it in
-   * the next iteration. The outer loop help to do this by reducing by one in every
-   * iteration.
-   *
-   * The inner loop compares the current element with the next element. If the current
-   * is greater (or smaller) the elements are swapped and we point to the next
-   * element. This is done until the end of the array minus one since we look ahead
-   * one element of the current, so there is no point in selecting the last element
-   * of the array.
-   *
-   * After looping through the entire array, we return it (its sorted at the end).
-   */
+
+  function swap(array, idx1, idx2) {
+    [array[idx1], array[idx2]] = [array[idx2], array[idx1]];
+  }
+
   for (let i = array.length - 1; i > 0; i--) {
+    /**
+     * To avoid making unnecesary scans over the array. We'll use an variable that tracks
+     * if we have done any swaps. If we haven't done any swaps in a complete scan, it means
+     * thata portion of the array is already sorted.
+     */
+    let didSwap = false;
+
     for (let j = 0; j < i; j++) {
       if (isAscending) {
-        if (array[j] > array[j + 1]) swap(array, j, j + 1);
+        if (array[j] > array[j + 1]) {
+          swap(array, j, j + 1);
+          didSwap = true;
+        }
       } else {
-        if (array[j] < array[j + 1]) swap(array, j, j + 1);
+        if (array[j] < array[j + 1]) {
+          swap(array, j, j + 1);
+          didSwap = true;
+        }
       }
     }
+
+    if (!didSwap) break;
   }
 
   return array;
