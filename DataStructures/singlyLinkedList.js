5bed5acb805953cb04aad65b406b58b3a42fd9ad
JD Villarroel <jdvillarroel@gmail.com>
2023-04-12 18:34:40 -0500

    Add push and pop methods to the implementation of the singly linked list.

--- DataStructures/singlyLinkedList.js
+++ DataStructures/singlyLinkedList.js
@@ -23,25 +23,65 @@ class sLinkedList {
     // Create new node to add.
     let newNode = new Node(_value);
 
-    // If list is empty, set head and tail to the new node.
+    /**
+     * If list is empty, set head and tail to the new node. Otherwise,
+     * make the tail to point to the new node, then make the new node
+     * the new tail. Increment the length and return the new list.
+     */
     if (this.head === null) {
       this.head = newNode;
       this.tail = this.head;
-
-      this.length++;
-
-      return;
-    }
-
-    if (this.length === 1) {
-      this.head.next = newNode;
+    } else {
+      this.tail.next = newNode;
       this.tail = newNode;
-      this.length++;
-      return;
     }
 
-    this.tail.next = newNode;
-    this.tail = newNode;
     this.length++;
+    return this;
+  }
+
+  // Remove the last element of the list.
+  pop() {
+    if (this.length === 0) return undefined;
+
+    /**
+     * If there's only one element set head and tail to null
+     * and return the value of the head.
+     */
+    if (this.length === 1) {
+      let temp = this.head.value;
+
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+
+      return temp;
+    }
+
+    /**
+     * Traverse the list keeping record of the previous node. When we get to the end,
+     * the next node of the previous is set to null (deleting the tail), and we make
+     * it the new tail.
+     */
+    let current = this.head;
+    let next = current.next;
+
+    while (next.next) {
+      current = current.next;
+      next = next.next;
+    }
+
+    current.next = null;
+    this.tail = current;
+    this.length--;
+
+    return next.value;
   }
 }
+
+let list = new sLinkedList();
+
+list.push(0);
+list.push(1);
+list.push(2);
+list.push(3);
