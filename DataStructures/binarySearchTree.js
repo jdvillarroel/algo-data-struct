4175a48c9720add245843172e440f47aba5c7d08
jdvillarroel <jdvillarroel@gmail.com>
2023-05-02 11:55:07 -0500

    Add initial implementation of the binary search tree data structure.

---
+++ DataStructures/binarySearchTree.js
@@ -0,0 +1,19 @@
+/**
+ * Implement a binary search tree.
+ */
+
+class Node {
+  constructor(_value) {
+    this.value = _value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
+
+const bst = new BinarySearchTree();
