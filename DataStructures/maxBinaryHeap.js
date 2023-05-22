5f1c23f52b7a796b7e36cf4f44e0612ffe553409
JD Villarroel <jdvillarroel@gmail.com>
2023-05-22 18:17:48 -0500

    Start method to remove max element in the heap. Still does not work.

--- DataStructures/maxBinaryHeap.js
+++ DataStructures/maxBinaryHeap.js
@@ -7,6 +7,11 @@ class maxBinaryHeap {
     this.values = [];
   }
 
+  /**
+   *
+   * @param {any} _value  Value to be stored in the heap.
+   * @returns List/array of values in the heap including the new added value.
+   */
   insert(_value) {
     // Add the new inserted value at the end of the list of values.
     this.values.push(_value);
@@ -34,8 +39,24 @@ class maxBinaryHeap {
 
     return this.values;
   }
+
+  removeMax() {
+    if (this.values.length === 0) return undefined;
+    if (this.values.length === 1) return this.values.pop();
+
+    // Swap first and last elements.
+    const max = this.values[0];
+    this.values[0] = this.values[this.values.length - 1];
+
+    // Save the element to bubble down.
+    let element = this.values[0];
+  }
 }
 
 const list = [41, 39, 33, 18, 27, 12, 55];
 
 const maxBHeap = new maxBinaryHeap();
+
+list.forEach((value) => {
+  maxBHeap.insert(value);
+});
