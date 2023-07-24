857c66892f27b5ac9444d57b0c8c1392b8a859cd
jdvillarroel <jdvillarroel@gmail.com>
2023-07-24 11:51:36 -0500

    same as previous commit.

--- DataStructures/hashTable.js
+++ DataStructures/hashTable.js
@@ -13,5 +13,13 @@ class HashTable {
   _hash(key) {
     let total = 0;
     const WEIRD_PRIME = 31;
+
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+
+    return total;
   }
 }
