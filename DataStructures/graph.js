a8160a13e6c5084525207031aa8ca794ca425181
JD Villarroel <jdvillarroel@gmail.com>
2023-07-25 18:00:44 -0500

    Added removeVertex and removeEgde methods to the implementation of the graph data structure.

--- DataStructures/graph.js
+++ DataStructures/graph.js
@@ -47,6 +47,58 @@ class Graph {
 
     return false;
   }
+
+  /**
+   * Remove the relationship between two vertex.
+   * @param {any} vertex1 Vertex or node to remove edge from.
+   * @param {any} vertex2 Vertex or node to remove edge from.
+   * @returns true if both vertex were valid and they were removed, false otherwise.
+   */
+  removeEdge(vertex1, vertex2) {
+    // Verify that both vertex exist before attempting to remove.
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      // Remove edge from vertex1.
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        (vertex) => vertex !== vertex2
+      );
+
+      // Remove edge from vertex2.
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (vertex) => vertex !== vertex1
+      );
+
+      return true;
+    }
+
+    return false;
+  }
+
+  /**
+   * Removes a vertex and any reference to that vertex in the list of edges in any other
+   * vertex.
+   * @param {any} vertex Vertex to be removed.
+   * @returns True if the vertex is valid and if it was removed successfully, false otherwise.
+   */
+  removeVertex(vertex) {
+    // Verify the vertex is being requested to remove exists in the graph.
+    if (this.adjacencyList[vertex]) {
+      /**
+       * Since we need to delete any reference to the vertex we are removing, we'll need
+       * to iterate over all the edges for that vertex and remove the edge that connects
+       * to the vertex we are removing.
+       */
+      this.adjacencyList[vertex].forEach((edge) => {
+        this.removeEdge(vertex, edge);
+      });
+
+      // Remove the vertex from the graph.
+      delete this.adjacencyList[vertex];
+
+      return true;
+    }
+
+    return false;
+  }
 }
 
 const g = new Graph();
@@ -63,3 +115,8 @@ let vertexList = [
 vertexList.forEach((vertex) => {
   g.addVertex(vertex);
 });
+
+g.addEdge("Caracas", "Pto La Cruz");
+g.addEdge("Caracas", "Merida");
+g.addEdge("Caracas", "Valencia");
+g.addEdge("Pto La Cruz", "Cumana");
